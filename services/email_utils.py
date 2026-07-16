6fd264256f140aa9c80c3d4b56d48105daf7b700
Abdulsalam <ireasoner02@gmail.com>
2026-07-16 08:24:41 +0100

    debugging email

--- services/email_utils.py
+++ services/email_utils.py
@@ -2,6 +2,9 @@ import aiosmtplib
 from email.message import EmailMessage
 from config import settings
 from jinja2 import Environment, FileSystemLoader
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 env = Environment(
@@ -33,26 +36,44 @@ async def send_reset_email(user_email, username, token):
 
   message.add_alternative(html_content, subtype="html")
 
+  logger.info("=== Starting send_reset_email ===")
+  logger.info(f"HOST: {settings.email_host}")
+  logger.info(f"PORT: {settings.email_port}")
+  logger.info(f"EMAIL: {settings.email_address}")
+
   import socket
 
-  print("HOST:", settings.email_host)
-  print("PORT:", settings.email_port)
-
   try:
-      print(socket.getaddrinfo(settings.email_host, settings.email_port))
-  except Exception as e:
-      print("DNS ERROR:", e)
+      result = socket.getaddrinfo(settings.email_host, settings.email_port)
+      logger.info(f"DNS RESULT: {result}")
+  except Exception:
+      logger.exception("DNS lookup failed")
 
-  await aiosmtplib.send(
-    message,
+  # await aiosmtplib.send(
+  #   message,
+  #   hostname=settings.email_host,
+  #   port=settings.email_port,
+  #   username=settings.email_address,
+  #   password=settings.email_password,
+  #   use_tls=True
+  # )
+
+  smtp = aiosmtplib.SMTP(
     hostname=settings.email_host,
     port=settings.email_port,
-    username=settings.email_address,
-    password=settings.email_password,
-    use_tls=True
-  )
+    use_tls=True,
+    timeout=30,
+)
 
+  logger.info("About to connect")
 
+  await smtp.connect()
+
+  logger.info("Connected!")
+
+  await smtp.quit()
+
+    
 
 
 
